198fcba6c946fb0b5db6516fb4831f3fc96dd181
LucasLartot <ldr.lucas@hotmail.fr>
2016-11-04 13:29:06 +0100

    Addition de deux entiers

--- calculator/main.c
+++ calculator/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addition (int a, int b)
+{
+    return a+b;
+}
 int main()
 {
-    printf("Hello world!\n");
+    printf("Le resultat de l'addition est : %d\n", addition (1,4));
     return 0;
 }
